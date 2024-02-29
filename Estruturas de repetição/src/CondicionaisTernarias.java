97b0c0c072ea2bc34b339e969cc7722da24db43c
Caco0 <cacotpds@gmail.com>
2024-02-29 09:12:50 -0300

    Ternary conditionals

---
+++ Estruturas de repetição/src/CondicionaisTernarias.java
@@ -0,0 +1,8 @@
+public class CondicionaisTernarias {
+    public static void main(String[] args) {
+        int nota = 7;
+        String resultado = nota >= 7 ? "Aprovado": nota >= 5 && nota < 7 ? "Recuperação" : "Reprovado";
+        
+        System.out.println("O aluno está " + resultado);
+    }
+}
